1d7ef879f647d50ae9849488e7b9bf4915eda3ff
StereoFlo <stereoflo@users.noreply.github.com>
2020-04-08 11:35:56 +0300

    post fix

---
+++ src/app/models/post.ts
@@ -0,0 +1,16 @@
+export class Post {
+  id: number;
+  name: string;
+  num: number;
+  comment: string;
+  files: [
+    {
+      display_name: string;
+      height: number;
+      width: number;
+      size: number;
+      path: string;
+      thumbnail: string;
+    }
+  ]|null;
+}
