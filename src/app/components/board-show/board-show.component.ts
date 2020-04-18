82c7bf1e810a7c2df434086fa7f7b90f1bb4d6f5
StereoFlo <stereoflo@users.noreply.github.com>
2020-04-18 14:27:00 +0300

    update board button

--- src/app/components/board-show/board-show.component.ts
+++ src/app/components/board-show/board-show.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DvachService} from '../../services/dvach.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Board} from '../../models/board';
@@ -11,8 +11,10 @@ import {Board} from '../../models/board';
 export class BoardShowComponent implements OnInit {
 
   board: Board;
+  isLoading = true;
 
-  constructor(private dvachService: DvachService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private dvachService: DvachService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
     if (!this.route.snapshot.params.id) {
@@ -22,9 +24,14 @@ export class BoardShowComponent implements OnInit {
 
     this.board = new Board();
 
-    this.dvachService.getBoardById(this.route.snapshot.params.id).subscribe(board => {
-      this.board = board;
-    });
+    this.loadBoard(this.route.snapshot.params.id);
   }
 
+  public loadBoard(boardId: string): void {
+    this.isLoading = true;
+    this.dvachService.getBoardById(boardId).subscribe(board => {
+      this.board = board;
+      this.isLoading = false;
+    });
+  }
 }
